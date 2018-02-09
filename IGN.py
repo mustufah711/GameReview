69cb8998ff0de209710300c3ae2556d1b7655fec
mustufah711 <mustufah711@gmail.com>
2018-02-09 13:05:04 -0500

    comments

--- IGN.py
+++ IGN.py
@@ -9,35 +9,48 @@ from time import time
 
 def main():
     
+    #Arrays to store the name, gamescore and genre for each game
     names = []
     game_score = []
     genre = []
     requests = 0
+    #Starting index for website
     j = '25'
     start_time = time()
- 
+    
+    #Run through each page and retrieve data for each game based on tag
     for i in range(1,4):
         print('iteration', '', j)
         header = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+        #Retrieve website url 
         response = get('http://www.ign.com/reviews/games?startIndex='+j, headers=header)
         print(response)
+        #Increment each value and then type cast to be parsed for next iteration
         j = int(j)+25
-        print(type(j), 'is it int?')
         j = str(j)
-        print(type(j))
+        #Between each request, go to sleep between 8 to 15 seconds to not overload server
         sleep(randint(8,15))
         requests+=1
         elapsed_time = time() - start_time
+        #Print how long each request takes
         print('Request: {}; Frequency: {} requests/s'.format(requests, requests/elapsed_time))
         clear_output(wait = True)
+        
+        #If request is not 200, print current request code
         if response.status_code != 200:
             warn('Request: {}; Status code: {}'.format(requests, response.status_code))    
+            
+        #Make sure request do not exceed certain limit
         if requests > 3:
             warn('Number of requests was greater than expected.')  
             break
+        
+        #Receive the response from website url and create an html parse
         html_soup = BeautifulSoup(response.text, 'html.parser')
+        #Find the ultimate div for each game info and start parsing
         game_info = html_soup.find_all('div', class_='clear itemList-item')
         
+        #Now we individually parse each name, rating and genre based on div
         for container in game_info:
             name = container.h3.a.text.replace('\n','').encode('utf-8').strip()
             names.append(name)
@@ -47,11 +60,12 @@ def main():
             game_type = container.find('span', class_='item-genre').text.replace('\n','').encode('utf-8').strip()
             genre.append(game_type)
     
+    #Store each entry into a panda DS and save it to CSV file since data deletes after every new request
     game_store = pd.DataFrame({'name': names,
                                'score': game_score,
                                'genres': genre})
+    #See if entry stroage was successful by viewing how many entries were saved
     print(game_store.info())
-    #print(game_store.values)
-    #print(game_store.head(50))
+    #Save to CSV
     game_store.to_csv('test2.csv')    
 main()
