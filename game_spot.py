460873bbb428bf8aa194b190ce00422d7aa17ee0
mustufah711 <mustufah711@gmail.com>
2018-02-09 12:18:13 -0500

    gamespot

--- game_spot.py
+++ game_spot.py
@@ -9,8 +9,17 @@ from time import time
 
 def main():
     header = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    url = 'https://www.polygon.com/games/ps4/reviewed'
+    url = 'https://www.gamespot.com/reviews/?page=1'
     response = get(url, headers=header)
     print(response)
+    
+    html_soup = BeautifulSoup(response.text, 'html.parser')
+    game_container = html_soup.find_all('article', class_='media media-game media-game')
+    print(len(game_container))
+    first_game = game_container[0]
+    #print(first_game)
+    
+    game_name = first_game.h3.find('div', class_='media-body')
+    print(game_name)
 
 main()
